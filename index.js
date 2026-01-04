a030949ca166f7d0da320953257733c451df9fb7
abdul rafay <abdulrafaycodeder@email.com>
2026-01-04 11:45:37 -0800

    coding chal rahe hai

---
+++ index.js
@@ -0,0 +1,29 @@
+// function makeYellow(){
+
+// }
+
+// var mainfull = document.getElementById("main").value
+
+// console.log(mainfull)
+
+// var fullinput = document.getElementById("input").innerHTML
+
+// console.log(fullinput)
+
+
+// function onclick(){
+    
+    
+// }
+
+
+function checkEmail(osamaa){
+  var chack = document.getElementById(osamaa).value
+  if(chack == "abdulrafay65748@gmail.com"){
+    alert("please enter email")    
+  }
+ 
+  console.log("chack",chack)
+  console.log("osamaa",osamaa)
+  
+}
